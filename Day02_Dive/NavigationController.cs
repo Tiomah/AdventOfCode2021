893185995c498b7a0e40768a6b9c140bc8711fd7
Tiomah <Tiomah38@gmail.com>
2022-03-23 19:53:02 +0900

    Simple solution for the first part of Day02

---
+++ Day02_Dive/NavigationController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+
+namespace Day02_Dive
+{
+    public static class NavigationController
+    {
+        public static (int, int) GetNavigationEndpoint(IEnumerable<string> navigationCommands)
+        {
+            (int, int) resultEndpoint = (0, 0);
+
+            foreach (var command in navigationCommands)
+            {
+                var splittedCommand = command.Split(' ');
+
+                switch (splittedCommand[0])
+                {
+                    case "forward":
+                        resultEndpoint.Item1 += int.Parse(splittedCommand[1]);
+                        break;
+                    case "up":
+                        resultEndpoint.Item2 -= int.Parse(splittedCommand[1]);
+                        if (resultEndpoint.Item2 < 0) resultEndpoint.Item2 = 0;
+                        break;
+                    case "down":
+                        resultEndpoint.Item2 += int.Parse(splittedCommand[1]);
+                        break;
+                }
+            }
+
+            return resultEndpoint;
+        }
+    }
+}
--- Day02_Dive/Program.cs
+++ Day02_Dive/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Day02_Dive.Properties;
+using System;
+using System.Collections.Generic;
 
 namespace Day02_Dive
 {
@@ -6,7 +8,16 @@ namespace Day02_Dive
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var testMovementsList = new List<string> { "forward 5", "down 5", "forward 8", "up 3", "down 8", "forward 2" };
+            var testEndpointResult = NavigationController.GetNavigationEndpoint(testMovementsList);
+
+            Console.WriteLine($"Test result is {testEndpointResult.Item1 * testEndpointResult.Item2}, must be 150");
+
+            var movementCommandsList = Resources.Movements;
+            var splittedCommands = movementCommandsList.Split('\n');
+            var endpointResult = NavigationController.GetNavigationEndpoint(splittedCommands);
+
+            Console.WriteLine($"Real result is {endpointResult.Item1 * endpointResult.Item2}");
         }
     }
 }
