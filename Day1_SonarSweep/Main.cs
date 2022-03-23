f8353eef2b69bb947270a10e89de430dd1d7a94d
Tiomah <Tiomah38@gmail.com>
2022-03-23 14:20:51 +0900

    Created Day1 project

---
+++ Day1_SonarSweep/Main.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Day1_SonarSweep
+{
+    class Day1_SonarSweep
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(args.Length);
+        }
+    }
+}
