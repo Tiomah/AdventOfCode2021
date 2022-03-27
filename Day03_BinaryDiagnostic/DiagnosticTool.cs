7c1caaa537b1ecf50dff0cd2f3b6a180bf138b72
Tiomah <Tiomah38@gmail.com>
2022-03-27 11:33:47 +0900

    Simple solution for the Day03 first part.

---
+++ Day03_BinaryDiagnostic/DiagnosticTool.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Day03_BinaryDiagnostic
+{
+    public static class DiagnosticTool
+    {
+        public static int GetPowerConsumption(IEnumerable<string> diagnosticData)
+        {
+            if (diagnosticData == null || !diagnosticData.Any())
+                return 0;
+
+            var columnData = Enumerable.Repeat(string.Empty, diagnosticData.First().Length).ToArray();
+
+            foreach (var diagnosticRow in diagnosticData)
+            {
+                for (int i = 0; i < diagnosticRow.Length; i++)
+                {
+                    columnData[i] += diagnosticRow[i];
+                }
+            }
+
+            var gammaRateCode = string.Empty;
+            var epsiloneRateCode = string.Empty;
+
+            foreach (var column in columnData)
+            {
+                var binaryValue = column.GroupBy(x => x).OrderByDescending(x => x.Count()).First().Key;
+                gammaRateCode += binaryValue;
+                epsiloneRateCode += Math.Abs(1 - char.GetNumericValue(binaryValue));
+            }
+
+            var gammaRate = Convert.ToInt32(gammaRateCode, 2);
+            var epsilonRate = Convert.ToInt32(epsiloneRateCode, 2);
+
+            var resultPowerConsumption = gammaRate * epsilonRate;
+
+            return resultPowerConsumption;
+        }
+    }
+}
---
+++ Day03_BinaryDiagnostic/Program.cs
@@ -0,0 +1,33 @@
+﻿using Day03_BinaryDiagnostic.Properties;
+using System;
+using System.Collections.Generic;
+
+namespace Day03_BinaryDiagnostic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var testDiagnosticData = new List<string> { 
+                "00100", "11110", "10110", "10111", "10101", "01111",
+                "00111", "11100", "10000", "11001", "00010", "01010"};
+            var testPowerConsumptionResult = DiagnosticTool.GetPowerConsumption(testDiagnosticData);
+
+            Console.WriteLine($"Test result is {testPowerConsumptionResult}, must be 198");
+
+            var diagnosticData = GetDiganosticDataFromResource();
+            var powerConsumptionResult = DiagnosticTool.GetPowerConsumption(diagnosticData);
+
+            Console.WriteLine($"Result is {powerConsumptionResult}");
+        }
+
+        static IEnumerable<string> GetDiganosticDataFromResource()
+        {
+            var resourceData = Resources.DiagnosticData;
+
+            var diagnosticData = resourceData.Split("\r\n");
+
+            return diagnosticData;
+        }
+    }
+}
